aa10d0dba9e03d1af3e468d1c5843bc19bd8e0a5
Bastian Pethel <bastianpethel360@gmail.com>
2025-09-17 21:52:18 -0400

    e2

--- exercises.py
+++ exercises.py
@@ -5,3 +5,11 @@ def calculate_area_triangle(base, height):
 
 # Test the function
 print('Exercise 1:', calculate_area_triangle(10, 5))
+
+# Exercise 2: Calculate Simple Interest
+def simple_interest(principal, rate, time):
+    interest = (principal * rate * time) / 100
+    return interest
+
+# Test the function
+print('Exercise 2:', simple_interest(1000, 5, 2))

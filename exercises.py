50c7c7f348d373600753d1daaf760ec9ca4dcb71
Bastian Pethel <bastianpethel360@gmail.com>
2025-09-17 21:54:20 -0400

    e6

--- exercises.py
+++ exercises.py
@@ -44,3 +44,15 @@ def sum_to(n):
 
 # Test the function
 print('Exercise 5:', sum_to(6))
+
+# Exercise 6: Find the Largest Number
+def largest(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+# Test the function
+print('Exercise 6:', largest(1, 2, 3))

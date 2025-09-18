ee1205ae746b251e89b8973af8eadfbf0ef6abec
Bastian Pethel <bastianpethel360@gmail.com>
2025-09-17 21:55:21 -0400

    e9

--- exercises.py
+++ exercises.py
@@ -74,3 +74,22 @@ def product(*args):
 
 # Test the function
 print('Exercise 8:', product(2, 5, 5))
+
+# Exercise 9: Basic Calculator
+def basic_calculator(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return None  # Avoid division by zero
+    else:
+        return None  # Invalid operation
+
+# Test the function
+print('Exercise 9 Result:', basic_calculator(10, 5, "subtract"))

d95a9f9e557cb13f4e1ea1e033c57a31780921dc
Bastian Pethel <bastianpethel360@gmail.com>
2025-09-17 21:51:32 -0400

    e1

---
+++ exercises.py
@@ -0,0 +1,7 @@
+# Exercise 1: Calculate Area of a Triangle
+def calculate_area_triangle(base, height):
+    area = (base * height) / 2
+    return area
+
+# Test the function
+print('Exercise 1:', calculate_area_triangle(10, 5))
